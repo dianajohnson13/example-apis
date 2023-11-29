83922e6b63dcd2785054c2837a3730228275ed3a
Diana <dianajohnson13@gmail.com>
2023-11-29 14:27:26 -0800

    Move password to env

--- src/db.js
+++ src/db.js
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
 import pg from 'pg';
 const { Pool } = pg;
 
+dotenv.config();
+
 const localPoolConfig = {
   user: 'postgres',
-  password: 'postgres',
+  password: process.env.DB_PASSWORD,
   host: 'localhost',
   port: 5432,
   database: 'exampledb'
