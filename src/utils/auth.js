54f6cbe6f2ad540b979a22babe495473f6dff9e9
Diana <dianajohnson13@gmail.com>
2023-12-01 13:32:04 -0800

    Extend token length

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export const makeTokens = ({ user_id, user_name, user_email }) => {
     const user = { user_id, user_name, user_email };
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '20s'});
-    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1hr'});
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1d'});
     return { accessToken, refreshToken };
 }
