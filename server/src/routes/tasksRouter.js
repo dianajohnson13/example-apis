3f293f5a475f6600d4dec680ad61dcf3e8de92dc
Diana <dianajohnson13@gmail.com>
2023-12-14 14:53:17 -0800

    Add GET /api/tasks/

--- server/src/routes/tasksRouter.js
+++ server/src/routes/tasksRouter.js
@@ -5,10 +5,31 @@ import {authenticateToken} from '../middleware/auth.js';
 // Routes under '/api/tasks'
 const router = express.Router();
 
+
+// gets user's tasks
+router.get('/', authenticateToken, async (req, res) => {
+  try {
+    const tasksData = await pool.query('SELECT * FROM tasks WHERE created_by = $1', [req.user.user_id]);
+
+    const tasks = tasksData.rows.map(task => {
+      return {
+        taskId: task.id,
+        title: task.title,
+        description: task.description,
+        completed: task.completed
+      }
+    });
+    
+    res.json({ tasks });
+
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
+
 // creates a new tast
 router.post('/', authenticateToken, async (req, res) => {
   try {
-console.log(req.user)
     const tasks = await pool.query(
       'INSERT INTO tasks (title,description,created_by) VALUES ($1,$2,$3) RETURNING *',
       [req.body.title, req.body.description, req.user.user_id]);
--- server/src/routes/usersRouter.js
+++ server/src/routes/usersRouter.js
@@ -27,6 +27,4 @@ router.post('/', async (req, res) => {
   }
 });
 
-
-
 export default router;
