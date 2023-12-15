5798bd288c3f9d673bb00603c8660b547b2713fa
Diana <dianajohnson13@gmail.com>
2023-12-15 12:43:36 -0800

    Add frontend for adding tasks

---
+++ client/src/api/Tasks.js
@@ -0,0 +1,52 @@
+import { getAccessToken } from '../utils/Auth';
+import { checkAuth } from './Auth';
+
+export const getUserTasks = async (retries = 0) => {
+    const resp = await fetch("/api/tasks", {
+      method: 'GET',
+      headers: {
+        "authorization": `Bearer ${getAccessToken()}`
+      }
+    });
+    if (resp.ok) {
+  
+      return resp.json().then(data => {
+        return data;
+      });
+    } else if (resp.status === 403) {
+      return checkAuth().then(() => {
+        if (retries === 0) {
+            return getUserTasks(1)
+        }
+  
+      });
+    } else {
+      return resp.json().then(data => {
+        throw new Error(data.error || "Something went wrong");
+      });
+    }
+  }
+
+  export const createTask = async (task, retries = 0) => {
+    const resp = await fetch("/api/tasks", {
+      method: 'POST',
+      headers: {
+        "authorization": `Bearer ${getAccessToken()}`,
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(task)
+    });
+  
+    if (resp.ok) {
+      return resp.json();
+    } else if (resp.unauthorized) {
+      return checkAuth().then(() => {
+        createTask(task);
+      });
+    } else {
+      return resp.json().then(data => {
+        throw new Error(data.error || "Something went wrong");
+      });
+    }
+  }
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,12 +1,87 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../containers/UserFetcher";
+import { getUserTasks, createTask } from "../api/Tasks";
 
 export default function Dashboard() {
     const { user } = useContext(UserContext);
-    
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState({title:"", description: ""});
+
+    const onAdd = (event) => {
+      event.preventDefault();
+      createTask(newTask).then((task) => {
+        setTasks([...tasks, task])
+        clearState();
+      })
+      .catch(error => {
+        console.log(error.error);
+      });
+    }
+
+    useEffect(() => {
+      getUserTasks()
+          .then((data) => {
+             setTasks(data.tasks);
+          })
+          .catch((error) => {
+              console.error(error)
+          });
+    }, [])
+
+    const clearState = () => {
+      setNewTask({title:"", description: ""});
+    }
+      
+    const handleTextInputChange = (event) => {
+      const { value, name } = event.target;
+  
+      switch (name) {
+        case "task-title":
+          setNewTask({...newTask, title: value});
+          break;
+        // case "task-description":
+        //   setNewTask({...newTask, description: value});
+      }
+    }
+
     return user ? (
-      <>
-          <h1>{`Welcome ${user.name}`}</h1>
-      </>
+      <div>
+          <h1 className="m-4 text-center">{user.name ? `Welcome ${user.name}!` : "Welcome!"}</h1>
+      
+          <div className="container mb-4">
+          <h2>Add a New Task</h2>
+            <form className="container" onSubmit={onAdd}>
+            <label htmlFor="task-title"><b>Task</b></label>
+                  <br/>
+              <div className="row">
+                <div className="col">
+
+                  <input className="form-control" type="text" name="task-title" value={newTask.title || ""} required onChange={handleTextInputChange} />
+                </div>
+                <div className="col-auto">
+                  <button className="btn btn-primary" role="submit">Add</button>
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div className="container">
+              <h2>My Tasks</h2>
+              {tasks.length === 0 ? <p>You don't have any tasks</p> : (
+
+                tasks.map(task => {
+                  return (
+                    <div className="container">
+                        <div className="list-group  mt-3 p-2 shadow">
+                          <strong>{task.title}</strong>
+                          <p>{task.description}</p>
+                        </div>
+                      </div>
+                  );
+                })
+              )}
+          </div>
+      </div>
     ) : null;
   }
