6b0002bc5a6e59ed90ca3cb94b8034c91c28fdb2
Diana <dianajohnson13@gmail.com>
2023-12-28 12:03:03 -0800

    Add GET api/tasks/:id

--- server/src/routes/tasksRouter.js
+++ server/src/routes/tasksRouter.js
@@ -31,6 +31,32 @@ router.get('/', authenticateToken, async (req, res) => {
   }
 });
 
+// gets a specific task
+router.get('/:id', authenticateToken, async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const tasks = await pool.query('SELECT * FROM tasks WHERE id = $1', [taskId]);
+    const task = tasks.rows[0];
+
+    if (!task) return res.status(404).json({error: "Task not found"});
+    if (task.created_by !== req.user.user_id) return res.status(403).json({error: "Task does not belong to authenticated user"});
+
+    res.json({
+      taskId: task.id,
+      title: task.title,
+      description: task.description,
+      completed: task.completed,
+      createdBy: task.created_by,
+      createdAt: task.created_at,
+      updatedAt: task.updated_at,
+      completedAt: task.completed_at
+    });
+
+  } catch (error) {
+    res.status(500).json({error: error.message});
+  }
+});
+
 // creates a new task
 router.post('/', authenticateToken, async (req, res) => {
   try {
